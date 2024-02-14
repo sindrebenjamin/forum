2157bec7e28e55ab4e766e0b519ce2fe543a8250
Alex Dalene <aledal51469@stud.noroff.no>
2024-02-14 13:49:09 +0100

    Adds CRUD operations

--- routes/dataRouter.js
+++ routes/dataRouter.js
@@ -1,8 +1,16 @@
 import { Router } from 'express';
-import { getData } from '../controllers/dataController.js';
+import {
+  getData,
+  postData,
+  deleteData,
+  updateData,
+} from '../controllers/dataController.js';
 
 const router = Router();
 
 router.get('/', getData);
+router.post('/create', postData);
+router.delete('/delete/:id', deleteData);
+router.put('/update/:id', updateData);
 
 export default router;
